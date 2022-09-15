98c150cc3ef6a3983ed4e0bfccde1b43c5fc217a
Thorsten Franz Brune <40182539+tb47@users.noreply.github.com>
2022-09-15 16:33:34 +0200

    Update init.js

--- 5/src/firebase/init.js
+++ 5/src/firebase/init.js
@@ -7,13 +7,9 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXsTlEyxZDg1F2FFPehjq6aNz4BxePvWA",
-  authDomain: "react-firebase-boilerpla-6c000.firebaseapp.com",
-  projectId: "react-firebase-boilerpla-6c000",
-  storageBucket: "react-firebase-boilerpla-6c000.appspot.com",
-  messagingSenderId: "971701796358",
-  appId: "1:971701796358:web:e13730870facaf05a54d87",
-  measurementId: "G-RZ1QMPC7L3"
+
+  // your cfg here
+  
 };
 
 // Initialize Firebase
