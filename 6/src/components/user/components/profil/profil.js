9babb263071859a1c7d4144ef3609be5220ea575
Thorsten Franz Brune <40182539+tb47@users.noreply.github.com>
2022-09-15 16:17:12 +0200

    Add files via upload

---
+++ 6/src/components/user/components/profil/profil.js
@@ -0,0 +1,10 @@
+function User_profil() {
+    return (
+        <div>
+            <p>User profil</p>
+            <label>Name:</label>
+            <input type="text"></input>
+        </div>
+    )
+};
+export default User_profil;
