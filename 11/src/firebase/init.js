f8cfb7572fffd47272d72e75d80c04ff63b6a0cf
Thorsten Franz Brune <40182539+tb47@users.noreply.github.com>
2022-09-21 14:31:43 +0200

    Add files via upload

---
+++ 11/src/firebase/init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  
+  // your config here
+
+};
+
+// Initialize Firebase
+// eslint-disable-next-line
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
